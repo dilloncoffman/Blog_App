5e4863b7c332e67aa89d8ee7126e0739c8fbf6cb
dilloncoffman <dillonscoffman@gmail.com>
2017-07-26 18:47:48 +0000

    Added the EDIT and UPDATE routes, handles UPDATE with method-override put request using an HTML form.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var express     = require('express'),
-    app         = express(),
-    bodyParser  = require('body-parser'),
-    mongoose    = require('mongoose');
+var express         = require('express'),
+    app             = express(),
+    bodyParser      = require('body-parser'),
+    mongoose        = require('mongoose'),
+    methodOverride  = require('method-override');
     
 // APP CONFIG
 mongoose.Promise = global.Promise;
@@ -9,6 +10,7 @@ mongoose.connect("mongodb://localhost/blog", {useMongoClient: true});
 app.set("view engine", "ejs");
 app.use(express.static("public")); //to serve custom stylesheet
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method")); //told app, when you get req as _method as param put whatever it is equal to and treat it as a PUT request or DELETE request
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -64,9 +66,30 @@ app.get("/blogs/:id", function(req, res) {
     });
 });
 
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+       if(err) {
+           res.redirect("/blogs");
+       } else {
+           res.redirect("/blogs/" + req.params.id);
+       }
+    });
+});
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is running..");
-})
+});
     
